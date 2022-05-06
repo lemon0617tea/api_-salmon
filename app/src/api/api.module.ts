eaa240a713f69948ba54f7d1c503b7eed39ab0bf
tkgstrator <nasawake.am@gmail.com>
2022-05-06 11:59:32 +0900

    - Fix Module and Makefile

--- app/src/api/api.module.ts
+++ app/src/api/api.module.ts
@@ -1,9 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ApiController } from './api.controller';
 import { ApiService } from './api.service';
-import { S2SController } from './s2s/s2s.controller';
-import { S2SService } from './s2s/s2s.service';
-import { S2SModule } from './s2s/s2s.module';
 import { ResultsModule } from './results/results.module';
 import { RankingModule } from './ranking/ranking.module';
 import { UsersController } from './users/users.controller';
@@ -13,14 +10,8 @@ import { UsersService } from './users/users.service';
 import { PrismaService } from 'src/prisma.service';
 
 @Module({
-  controllers: [ApiController, S2SController, UsersController],
-  providers: [ApiService, S2SService, UsersService, PrismaService],
-  imports: [
-    S2SModule,
-    ResultsModule,
-    RankingModule,
-    UsersModule,
-    SchedulesModule,
-  ],
+  controllers: [ApiController, UsersController],
+  providers: [ApiService, UsersService, PrismaService],
+  imports: [ResultsModule, RankingModule, UsersModule, SchedulesModule],
 })
 export class ApiModule {}
